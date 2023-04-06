40b4242f638ab0b4299ea49734037f3b10bb5f75
metalboyrick <rsulisthio@gmail.com>
2023-04-06 23:08:39 +0700

    chore: add linter config

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import { useState } from 'react'
 import { Box, Button } from '@chakra-ui/react'
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <Box>
-      <Button onClick={() => setCount(count + 1)}>
-        Count is {count}
-      </Button>
+      <Button onClick={() => setCount(count + 1)}>Count is {count}</Button>
     </Box>
   )
 }
